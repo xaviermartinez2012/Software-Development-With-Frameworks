66a6fb058389991dd0f2bb3fe7218ffff6abff87
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2018-02-05 01:57:50 -0800

    Poker Face implements PokerHand class

--- PokerFace/src/Cecs475.Poker.Model/PokerHand.cs
+++ PokerFace/src/Cecs475.Poker.Model/PokerHand.cs
@@ -1,9 +1,64 @@
+using System;
+using System.Collections.Generic;
+
 namespace Cecs475.Poker.Model {
 /// <summary>
 /// Represents a Poker hand made up of Cards.
 /// </summary>
-    public class PokerHand
+    public class PokerHand : IComparable<PokerHand>
     {
-        
+        public enum HandType {
+            HighCard = 1,
+            Pair,
+            TwoPair,
+            ThreeOfKind,
+            Straight,
+            Flush,
+			FullHouse,
+			FourOfKind,
+			StraightFlush,
+			RoyalFlush
+		}
+
+        /// <summary>
+		/// The cards in hand (in ascending order).
+		/// </summary>
+		public List<Card> Hand {
+			get;
+			private set;
+		}
+
+        /// <summary>
+		/// The "kind" of hand represented.
+		/// </summary>
+		public HandType PokerHandType {
+			get;
+			private set;
+		}
+
+        public PokerHand(List<Card> hand, HandType pokerHandType) {
+            PokerHandType = pokerHandType;
+            Hand = hand;
+            SortHand();
+        }
+
+        public void SortHand() {
+            Hand.Sort((c1, c2) => c1.CompareTo(c2));
+        }
+
+        public int CompareTo(PokerHand other) {
+            int compareHandType = this.PokerHandType.CompareTo(other.PokerHandType);
+            if (compareHandType == 0)
+            {
+                for (int i = 0; i < other.Hand.Count; i++)
+                {
+                    compareHandType = this.Hand[i].CompareTo(other.Hand[i]);
+                    if (compareHandType != 0) {
+                        return compareHandType;
+                    }
+                }
+            }
+            return compareHandType;
+        }
     }
 }
