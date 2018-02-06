9018d9d6596df6453d1eb9761869570c0cfd0393
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2018-02-05 19:58:54 -0800

    Poker Face code reformat

--- PokerFace/src/Cecs475.Poker.Model/PokerHandClassifier.cs
+++ PokerFace/src/Cecs475.Poker.Model/PokerHandClassifier.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
-namespace Cecs475.Poker.Cards
-{
-    public class PokerHandClassifier
-    {
-        public static PokerHand ClassifyHand(IEnumerable<Card> hand) {
+namespace Cecs475.Poker.Cards {
+public class PokerHandClassifier {
+public static PokerHand ClassifyHand(IEnumerable<Card> hand) {
+    PokerHand classifiedHand = new PokerHand(new List<Card>(hand), PokerHand.HandType.HighCard);
 
-            PokerHand classifiedHand = new PokerHand(new List<Card> (hand), PokerHand.HandType.HighCard);
-            return classifiedHand;
-        }
-    }
+    return classifiedHand;
+}
+}
 }
