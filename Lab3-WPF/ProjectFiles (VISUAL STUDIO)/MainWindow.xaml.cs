37cb2b649f405a00aae07bcbb2d1089b485014b6
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2018-03-15 09:00:26 -0700

    Lab3 finished implementation of lab3

---
+++ Lab3-WPF/ProjectFiles (VISUAL STUDIO)/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            String maximizeButtonOriginalContent = (String) maximizeButton.Content;
+            maximizeButton.Click += (s, e) =>
+            {
+                switch (Application.Current.MainWindow.WindowState)
+                {
+                    case WindowState.Normal:
+                        Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                        maximizeButton.Content = "Unmaximize!";
+                        break;
+                    default:
+                        Application.Current.MainWindow.WindowState = WindowState.Normal;
+                        maximizeButton.Content = maximizeButtonOriginalContent;
+                        break;
+                }
+            };
+        }
+    }
+}
