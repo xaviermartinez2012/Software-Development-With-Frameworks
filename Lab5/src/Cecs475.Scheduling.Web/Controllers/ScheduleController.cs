df88c40270e63e0b0d6e56c28b720c342d7b1a22
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2018-05-06 13:53:06 -0700

    implements GET /api/schedule/{year}/{term}

---
+++ Lab5/src/Cecs475.Scheduling.Web/Controllers/ScheduleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Cecs475.Scheduling.Web.Controllers
+{
+    [RoutePrefix("api/schedule")]
+    public class ScheduleController : ApiController
+    {
+        private Model.CatalogContext mContext = new Model.CatalogContext();
+
+        [HttpGet]
+        [Route("{year}/{term}")]
+        public IEnumerable<CourseSectionDto> GetCourse(string year, string term)
+        {
+            var semester = mContext.SemesterTerms.Where(s => s.Name.Equals(String.Format("{0} {1}", term, year), StringComparison.InvariantCultureIgnoreCase))
+                .SingleOrDefault();
+            if (semester == null)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(
+                HttpStatusCode.NotFound, $"No semester term with term + year {term} {year} found"));
+            }
+            return semester.CourseSections.Select(CourseSectionDto.From);
+        }
+    }
+}
