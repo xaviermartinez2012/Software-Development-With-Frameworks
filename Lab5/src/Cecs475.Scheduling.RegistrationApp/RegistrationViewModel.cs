088d4d34030fbab69984e73fe43430737b9a9e79
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2018-05-06 15:19:08 -0700

    Lab5: adds SemesterTermDto and CourseSectionDto objects to RegistrationViewModel

--- Lab5/src/Cecs475.Scheduling.RegistrationApp/RegistrationViewModel.cs
+++ Lab5/src/Cecs475.Scheduling.RegistrationApp/RegistrationViewModel.cs
@@ -4,15 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cecs475.Scheduling.RegistrationApp {
-	public class RegistrationViewModel {
-		/// <summary>
-		/// A URL path to the registration web service.
-		/// </summary>
-		public string ApiUrl { get; set; }
-		/// <summary>
-		/// The full name of the student who is registering.
-		/// </summary>
-		public string FullName { get; set; }
-	}
+namespace Cecs475.Scheduling.RegistrationApp
+{
+
+    public class SemesterTermDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+
+    public class CourseSectionDto
+    {
+        public int Id { get; set; }
+        public int SemesterTermId { get; set; }
+        public Catalogcourse CatalogCourse { get; set; }
+        public int SectionNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CatalogCourse.DepartmentName} {CatalogCourse.CourseNumber}-{SectionNumber}";
+        }
+    }
+
+    public class Catalogcourse
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public string CourseNumber { get; set; }
+        public object[] PrerequisiteCourseIds { get; set; }
+    }
+
+    public class RegistrationViewModel
+    {
+        /// <summary>
+        /// A URL path to the registration web service.
+        /// </summary>
+        public string ApiUrl { get; set; }
+        /// <summary>
+        /// The full name of the student who is registering.
+        /// </summary>
+        public string FullName { get; set; }
+    }
 }
